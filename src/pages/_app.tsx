194f26e0c36ca15e75d3533779aff18576b5a6ed
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-05-29 12:57:11 +0530

    🎉 update `_app`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>LeetCode</title>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+        <meta name="description" content="LeetCode website clone" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
