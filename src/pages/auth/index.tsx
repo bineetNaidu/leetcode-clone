86f2f270ed97007909dfcde1a89773e9792f9469
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-05-29 17:52:17 +0530

    🚀 add auth guard check in auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,10 +1,25 @@
 import { authModalAtom } from '@/atoms/authModal.atom';
 import { Navbar } from '@/components/Navbar';
 import AuthModal from '@/components/modals/AuthModal';
+import { auth } from '@/lib/firebase';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilValue } from 'recoil';
 
 const Auth = () => {
   const authModal = useRecoilValue(authModalAtom);
+  const [authUser, isLoading] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (authUser) router.push('/');
+    if (!isLoading && !authUser) setPageLoading(false);
+  }, [authUser, isLoading, router]);
+
+  if (pageLoading) return null;
+
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
