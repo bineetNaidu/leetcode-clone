2dea43caf82eda5f5b1fa7af94b7535ed811340a
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-05-30 16:52:24 +0530

    🪨 add ResetPassword Logic

--- src/components/modals/ResetPassword.tsx
+++ src/components/modals/ResetPassword.tsx
@@ -1,8 +1,33 @@
-import { FC } from 'react';
+import { auth } from '@/lib/firebase';
+import { FC, useState } from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 
 export const ResetPassword: FC = () => {
+  const [email, setEmail] = useState('');
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const success = await sendPasswordResetEmail(email);
+    if (success) {
+      setEmail('');
+      alert('Password reset email sent!');
+    } else {
+      console.error(error);
+      alert(error);
+    }
+  };
+
+  const handleEmailFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8">
+    <form
+      className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8"
+      onSubmit={handleSubmit}
+    >
       <h3 className="text-xl font-medium  text-white">Reset Password</h3>
       <p className="text-sm text-white ">
         Forgotten your password? Enter your e-mail address below, and we&apos;ll
@@ -16,20 +41,24 @@ export const ResetPassword: FC = () => {
           Your email
         </label>
         <input
+          required
           type="email"
           name="email"
           id="email"
           className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
           placeholder="name@company.com"
+          value={email}
+          onChange={handleEmailFieldChange}
         />
       </div>
 
       <button
         type="submit"
+        disabled={sending}
         className={`w-full text-white  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
-                bg-brand-orange hover:bg-brand-orange-s `}
+                bg-brand-orange hover:bg-brand-orange-s disabled:bg-gray-400 disabled:cursor-not-allowed`}
       >
-        Reset Password
+        {sending ? 'Sending...' : 'Send password reset email'}
       </button>
     </form>
   );
