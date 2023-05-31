f270d7ac7ced3b9a54bd14047d1269b13ccde17b
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-05-31 13:16:45 +0530

    👾 update Topbar component to be re-useable

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -4,13 +4,22 @@ import { FC } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/lib/firebase';
 import { LogoutBtn } from './LogoutBtn';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { BsList } from 'react-icons/bs';
+import { Timer } from './Timer';
 
-export const Topbar: FC = () => {
+type Props = {
+  problemPage?: boolean;
+};
+
+export const Topbar: FC<Props> = ({ problemPage }) => {
   const [authUser] = useAuthState(auth);
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
-        className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}
+        className={`flex w-full items-center justify-between ${
+          problemPage && 'max-w-[1200px] mx-auto'
+        }`}
       >
         <Link href="/" className="h-[22px] flex-1">
           <Image
@@ -21,6 +30,28 @@ export const Topbar: FC = () => {
             width={100}
           />
         </Link>
+
+        {problemPage && (
+          <div className="flex items-center gap-4 flex-1 justify-center">
+            <div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+              <FaChevronLeft className="text-brand-orange" />
+            </div>
+
+            <Link
+              href="/"
+              className="flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer"
+            >
+              <div>
+                <BsList className="text-brand-orange" />
+              </div>
+              <p>Problem List</p>
+            </Link>
+
+            <div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+              <FaChevronRight className="text-brand-orange" />
+            </div>
+          </div>
+        )}
 
         <div className="flex items-center space-x-4 flex-1 justify-end">
           <div>
@@ -33,13 +64,15 @@ export const Topbar: FC = () => {
               Premium
             </a>
           </div>
-          {!authUser ? (
+          {!authUser && (
             <Link href="/auth">
               <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
                 Sign In
               </button>
             </Link>
-          ) : (
+          )}
+          {problemPage && <Timer />}
+          {authUser && (
             <div className="cursor-pointer group relative">
               <Image
                 src="/avatar.png"
