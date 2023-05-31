faf2115906f615adaccd979d04b91051c74205ca
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-05-31 22:59:38 +0530

    🚀 add preference Nav

---
+++ src/components/Playground.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { PreferenceNav } from './PreferenceNav';
+
+export const Playground: FC = () => {
+  return (
+    <div>
+      <PreferenceNav />
+    </div>
+  );
+};
--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -1,12 +1,13 @@
 import Split from 'react-split';
 import { FC } from 'react';
 import { ProblemDescription } from './ProblemDescription';
+import { Playground } from './Playground';
 
 export const Workspace: FC = () => {
   return (
     <Split className="split" minSize={0}>
       <ProblemDescription />
-      <div>code editor</div>
+      <Playground />
     </Split>
   );
 };
