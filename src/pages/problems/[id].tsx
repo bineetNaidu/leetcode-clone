ef10d6f22f2ef56bc615853c2231debf2d1efdf0
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-05-31 13:15:52 +0530

    🎉 add base Problem page

---
+++ src/pages/problems/[id].tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Topbar } from '@/components/Topbar';
+
+const Problem: NextPage = () => {
+  return (
+    <div>
+      <Topbar problemPage />
+    </div>
+  );
+};
+
+export default Problem;
