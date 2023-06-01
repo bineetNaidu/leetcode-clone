1c8194deb980a4dc58f76ae3de8d9b04f98234f0
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-06-01 17:00:44 +0530

    🚀 add test case UI

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -23,7 +23,40 @@ export const Playground: FC = () => {
             style={{ fontSize: 14, height: '100%' }}
           />
         </div>
-        <div>test case</div>
+        <div className="w-full px-5 overflow-auto">
+          <div className="flex h-10 items-center space-x-6">
+            <div className="relative flex h-full flex-col justify-center cursor-pointer">
+              <h2 className="text-sm font-medium text-white leading-5">
+                Test Cases
+              </h2>
+              <hr className="absolute bottom-0 w-full h-0.5 rounded-full border-none bg-white" />
+            </div>
+          </div>
+
+          <div className="flex">
+            {[1, 2, 3].map((i) => (
+              <div className="mr-2 items-center mt-2 text-white" key={i}>
+                <div className="flex flex-wrap items-center gap-y-4">
+                  <button className="flex-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                    Case {i}
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="font-semibold my-4">
+            <p className="text-sm font-medium mt-4 text-white">Input:</p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              nums: [1,2,3,4,5,6,7,8,9,10], k: 3
+            </div>
+
+            <p className="text-sm font-medium mt-4 text-white">Output:</p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              [1,9,10]
+            </div>
+          </div>
+        </div>
       </Split>
     </div>
   );
